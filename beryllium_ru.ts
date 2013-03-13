e2f52f714a638ac829dc4308ee2dbd619390ba9b
Pavel Bludov <pbludov@gmail.com>
2013-03-13 10:37:20 +0000

    localization fixed

--- beryllium_ru.ts
+++ beryllium_ru.ts
@@ -4,30 +4,15 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="110"/>
+        <location filename="mainwindow.cpp" line="106"/>
         <source>Take
 snapshot</source>
         <translation>Сделать
 снимок</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="688"/>
-        <source>on</source>
-        <translation>вкл</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="688"/>
-        <source>off</source>
-        <translation>выкл</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="690"/>
-        <source>Recording of entire study is %1</source>
-        <translation>Запись всего исследования %1</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="126"/>
-        <source>To start an study press &quot;Start&quot; button.
+        <location filename="mainwindow.cpp" line="122"/>
+        <source>To start a study press &quot;Start&quot; button.
 
 During the study you can take snapshots and save video clips.</source>
         <translation>Чтобы начать исследование, нажмите кнопку &quot;начать&quot;.
@@ -35,72 +20,87 @@ During the study you can take snapshots and save video clips.</source>
 Во время исследования вы можете делать снимки и сохранять видео клипы.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="156"/>
-        <location filename="mainwindow.cpp" line="735"/>
+        <location filename="mainwindow.cpp" line="697"/>
+        <source>on</source>
+        <translation>вкл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="697"/>
+        <source>off</source>
+        <translation>выкл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="699"/>
+        <source>Recording of entire study is %1</source>
+        <translation>Запись всего исследования %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="152"/>
+        <location filename="mainwindow.cpp" line="743"/>
         <source>Default</source>
         <translation>По умолчанию</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="170"/>
+        <location filename="mainwindow.cpp" line="166"/>
         <source>&amp;Menu</source>
         <translation>&amp;Меню</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="172"/>
+        <location filename="mainwindow.cpp" line="168"/>
         <source>&amp;About Qt</source>
         <translation>&amp;О библиотеке Qt</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="174"/>
+        <location filename="mainwindow.cpp" line="170"/>
         <source>&amp;Enable RTP streaming</source>
         <translation>Включить RTP &amp;поток</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="178"/>
+        <location filename="mainwindow.cpp" line="174"/>
         <source>&amp;Record entire study</source>
         <translation>Записывать исследование &amp;целиком</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="182"/>
+        <location filename="mainwindow.cpp" line="178"/>
         <source>E&amp;xit</source>
         <translation>В&amp;ыход</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="186"/>
+        <location filename="mainwindow.cpp" line="182"/>
         <source>&amp;Profiles</source>
         <translation>&amp;Профили</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="188"/>
+        <location filename="mainwindow.cpp" line="184"/>
         <source>&amp;Default</source>
         <translation>&amp;По умолчанию</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="652"/>
+        <location filename="mainwindow.cpp" line="546"/>
         <source>Failed to load image %1</source>
         <translation>Не удалось загрузить снимок %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="669"/>
+        <location filename="mainwindow.cpp" line="678"/>
         <source>Stop
 study</source>
         <translation>Закончить
 исследование</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="669"/>
+        <location filename="mainwindow.cpp" line="678"/>
         <source>Start
 study</source>
         <translation>Начать
 исследование</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="681"/>
+        <location filename="mainwindow.cpp" line="690"/>
         <source>Pause</source>
         <translation>Пауза</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="681"/>
+        <location filename="mainwindow.cpp" line="690"/>
         <source>Record</source>
         <translation>Запись</translation>
     </message>
