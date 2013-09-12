82a6ca5ee4b2ff7a84a1ec38a11de8861ebc8fe5
Pavel Bludov <pbludov@gmail.com>
2013-09-12 08:41:27 +0000

    debian dicom fix

--- build-pkg.sh
+++ build-pkg.sh
@@ -9,7 +9,7 @@ for arg in "$@"; do
 done
 
 # Remove non-free code
-if [ -z $dicom ]; then
+if [ $dicom != 0 ]; then
   for f in $(grep -l WITH_DICOM *.h *.cpp)
     do unifdef -o $f -UWITH_DICOM $f
   done
