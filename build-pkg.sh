e93707185e3941b732ee74c88cc9dff2f97e61fc
Pavel Bludov <pbludov@gmail.com>
2013-09-12 04:24:21 +0000

    Debian autobuild system

--- build-pkg.sh
+++ build-pkg.sh
@@ -1,1 +1,19 @@
-lrelease beryllium/*.ts && tar czf beryllium.tar.gz beryllium && rpmbuild -ta beryllium.tar.gz
+#!/bin/bash
+
+# Localizations
+lrelease *.ts
+
+distro=$(lsb_release -is)
+case $distro in
+Ubuntu | Debian)  echo "Building DEB package"
+    cp docs/* debian/
+    dpkg-buildpackage -I.svn -rfakeroot 
+    ;;
+"openSUSE project" | fedora)  echo "Building RPM package"
+    cd .. && tar -X.svn czf beryllium.tar.gz beryllium --exclude=.svn && rpmbuild -ta beryllium.tar.gz
+    ;;
+*) echo "$distro is not supported yet"
+   ;;
+esac
+
+
