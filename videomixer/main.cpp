77537ba62474437d812e0aea8e54690a86b131fc
Pavel Bludov <pbludov@gmail.com>
2014-06-17 12:32:41 +0900

    Video mixing daemon.

---
+++ videomixer/main.cpp
@@ -0,0 +1,58 @@
+#include "mixer.h"
+
+#include <signal.h>
+#include <QCoreApplication>
+#include <QDebug>
+#include <QSettings>
+#include <QStringList>
+
+#include <QGst/Init>
+#include <QGlib/Error>
+
+#define MAX_GROUPS 32
+
+void sighandler(int)
+{
+    qApp->quit();
+}
+
+int main(int argc, char *argv[])
+{
+    int errCode;
+
+    // QGStreamer stuff
+    //
+    QGst::init(&argc, &argv);
+
+    QCoreApplication::setOrganizationName("dc.baikal.ru");
+    QCoreApplication::setApplicationName("videomixer");
+    QCoreApplication app(argc, argv);
+
+    QSettings settings;
+    QList<Mixer*> mixers;
+
+    Q_FOREACH (auto group, settings.childGroups())
+    {
+        qDebug() << group;
+
+        try
+        {
+            mixers.push_back(new Mixer(group));
+        }
+        catch (const QGlib::Error& ex)
+        {
+            qCritical() << group << " error:" << ex.message();
+        }
+    }
+
+    signal(SIGINT, sighandler);
+
+    errCode = app.exec();
+    Q_FOREACH (auto mixer, mixers)
+    {
+        delete mixer;
+    }
+
+    QGst::cleanup();
+    return errCode;
+}
