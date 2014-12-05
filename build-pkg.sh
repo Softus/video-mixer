f96b7c2d20a9d2fadc6a7380c391425438c1a534
Pavel Bludov <pbludov@gmail.com>
2014-12-05 15:12:37 +0800

    build script fix

--- build-pkg.sh
+++ build-pkg.sh
@@ -5,7 +5,6 @@ lrelease *.ts
 
 dicom=0
 debug=0
-touch=1
 
 for arg in "$@"; do
   case $arg in
@@ -15,14 +14,8 @@ for arg in "$@"; do
   'debug')
     debug=1
      ;;
-  'touch')
-     touch=1
-     ;;
-  'notouch')
-    touch=0
-     ;;
   *) echo "Unknown argument $arg"
-     echo "Valid arguments are debug dicom notouch"
+     echo "Valid arguments are debug dicom"
      ;;
   esac
 done
@@ -46,11 +39,6 @@ if [ $debug == 1 ]; then
   ' beryllium.pro
 fi
 
-if [ $touch == 1 ]; then
-  sed -i '1 i CONFIG+=touch
-  ' beryllium.pro
-fi
-
 distro=$( (lsb_release -is || echo windows) | awk '{print tolower($1)}' )
 rev=$( lsb_release -rs || (cmd.exe /c ver | gawk 'match($0,/[0-9]\.[0-9]/){print substr($0,RSTART,RLENGTH)}') )
 case $distro in
@@ -66,7 +54,7 @@ ubuntu | debian)  echo "Building DEB package"
     ;;
 opensuse | fedora | suse | centos)  echo "Building RPM package"
     rm -f ../*.rpm ../*.tar.gz
-    tar czf ../beryllium.tar.gz * --exclude=.git --exclude=*.sh && rpmbuild -D"dicom $dicom" -D"debug $debug" -D"touch $touch" -D"distro $distro" -D"rev $rev" -ta ../beryllium.tar.gz
+    tar czf ../beryllium.tar.gz * --exclude=.git --exclude=*.sh && rpmbuild -D"dicom $dicom" -D"debug $debug" -D"distro $distro" -D"rev $rev" -ta ../beryllium.tar.gz
     mv ~/rpmbuild/RPMS/*-beryllium-*.rpm ..
     ;;
 windows)  echo "Building MSI package"
